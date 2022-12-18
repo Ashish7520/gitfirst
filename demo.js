9f6ecd3dad219e7b5267044776a911dea7de19b3
Ashish7520 <ashishnandwana2@gmail.com>
2022-12-18 16:29:44 +0530

    bind

--- demo.js
+++ demo.js
@@ -1,18 +1,10 @@
-var obj={
-    num : 22
+var student={
+    age:20
 }
 
-var addToThis = function (a,b,c) {
-
-    return this.num+a+b+c
-
+var ageOfStudent = function(){
+    return this.age
 }
 
-console.log(addToThis.call(obj,1,2,3))
-
-var arr=[1,2,3]
-
- console.log(addToThis.apply(obj,arr))
-
-var bound= addToThis.bind(obj)
-console.log(bound(1,2,3))
+var bound=ageOfStudent.bind(student)
+console.log(bound())
