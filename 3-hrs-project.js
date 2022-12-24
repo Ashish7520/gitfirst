4faa7cf4afbc332347460b45abb23e081c16e918
Ashish7520 <ashishnandwana2@gmail.com>
2022-12-24 20:11:47 +0530

    callback

--- 3-hrs-project.js
+++ 3-hrs-project.js
@@ -1,20 +1,2 @@
 
 
-var form=document.getElementById('formId')
-var itemList=document.getElementById('items')
-
-form.addEventListener('submit', addItem)
-
-function addItem(event){
-    event.preventDefault();
-
-    var newItem= document.getElementById('expense').value
-    var description = document.getElementById("description").value
-    var catagory = document.getElementById("catagory").value
-
-    var li=document.createElement('li')
-    li.className='list-group-item'
-    let totalDescriptionOfTheExpence = `${newItem}-${description}-${catagory} `
-    li.appendChild(document.createTextNode(totalDescriptionOfTheExpence))
-    itemList.appendChild(li)
-}
---
+++ dynamic registration form/callback.js
@@ -0,0 +1,30 @@
+const posts=[
+    {title: 'post one', body:'this is the first post' , createdAt:new Date().getTime()},
+    {title: 'post two', body: ' this is the second post', createdAt:new Date().getTime()}
+];
+
+function getPosts(){
+    setInterval(()=>{
+        let output=''
+        posts.forEach((post, index)=>{
+            output+= `<li> ${post.title} - last updated ${(new Date()-post.createdAt)/1000} seconds ago</li>`
+        })
+        document.body.innerHTML=output
+
+    }, 1000)
+}
+
+function createPost (post, callback){
+    setTimeout(()=>{
+
+        posts.push({...post, createdAt:new Date().getTime()});
+        callback();
+
+    }, 2000)
+}
+
+createPost(
+    {title:'post three', body:'this is third post'}, getPosts
+)
+
+createPost({title:'post four', body:'this is fourth post'}, getPosts)
